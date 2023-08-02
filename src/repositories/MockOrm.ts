87545bc2357458ab34362d67fa27f09c59434023
marharita08 <rozgon.margarita08@gmail.com>
2023-08-02 16:29:14 +0300

    add notes repository

---
+++ src/repositories/MockOrm.ts
@@ -0,0 +1,25 @@
+import jsonfile from 'jsonfile';
+import {INote} from "../model/Note";
+
+const DB_FILE_NAME = 'database.json';
+
+
+interface IDb {
+    notes: INote[],
+}
+
+function openDb(): Promise<IDb> {
+    return jsonfile.readFile(__dirname + '/' + DB_FILE_NAME) as Promise<IDb>;
+}
+
+function saveDb(db: IDb): Promise<void> {
+    return jsonfile.writeFile((__dirname + '/' + DB_FILE_NAME), db);
+}
+
+
+// **** Export default **** //
+
+export default {
+    openDb,
+    saveDb,
+} as const;
